fc581043d9c1f1159d7eef94b0bc55b7dac9fa0a
root <root@localhost>
2023-05-03 21:19:13 +0800

    auto publish 1683119952987

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+
+ //Header file for input output function
+ #include<iostream>
+ 
+ using namesapce std;
+ 
+ // main function -
+ // where the execution of program begins
+ int main()
+ {
+    int num1;
+    int num2;
+    
+    cout << "First number: ";
+    cin >> num1;   
+    cout >> "Second number: ";
+    cin >> num2;
+   
+    cout << "Result: " << num1 + num2;
+    return 0;
+ }
